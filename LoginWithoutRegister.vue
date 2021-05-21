dbbd73737bf4eaad18c5d8edcba29ceef488c73b
BargasoRC <renanbargaso16@gmail.com>
2021-05-21 13:41:17 +0800

    login without register

--- LoginWithoutRegister.vue
+++ LoginWithoutRegister.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="col-sm-12 col-md-6 col-lg-4 mx-auto holder">
-    <div class="login-wrapper">
-      <div class="login-header" style="margin-top: 75px;">
-        <img :src="require('src/assets/img/logo.png')" v-on:click="redirect('/')">
+  <div class="login-container">
+    <div class="login-wrapper col-12 col-sm-12 col-lg-8 mx-auto">
+      <div class="login-header">
+        <img :src="require('src/assets/img/logo.png')" v-on:click="redirect('/')" />
       </div>
-      <span style="width:100%;float:left;text-align:center;font-size:20px;margin-bottom:20px;">
-        Login to <b class="text-primary">{{common.APP_NAME}}</b>
-      </span>
       <div class="login-message-holder login-spacer text-center" v-if="errorMessage != null">
         <span class="text-danger text-center"><b>Oops!</b> {{errorMessage}}</span>
       </div>
-      <div>
-        <div class="input-group login-spacer">
-          <span class="input-group-addon" id="addon-1"><i class="fa fa-user"></i></span>
+      <span style="width: 100%; float: left; text-align: center; font-size: 20px; margin-bottom: 65px; margin-top: 20px;">
+        <b class="text-white h1">{{common.APP_ADDRESS}}</b>
+      </span>
+      <div class="col-sm-4 col-md-4 col-xs-4 col-lg-4 mx-auto">
+        <div class="input-group login-spacer mb-4">
+          <!-- <span class="input-group-addon" id="addon-1"><i class="fa fa-user"></i></span> -->
           <div class="input-group">
-            <input type="text" class="form-control form-control-login" placeholder="Username or Email" aria-describedby="addon-1" v-model="username">
+            <input type="text" class="form-control form-control-login" placeholder="Usercode" aria-describedby="addon-1" v-model="username">
           </div>
         </div>
-        <div class="input-group login-spacer">
-          <span class="input-group-addon" id="addon-2"><i class="fa fa-key"></i></span>
+        <div class="input-group login-spacer mt-4 mb-4">
+          <!-- <span class="input-group-addon" id="addon-2"><i class="fa fa-key"></i></span> -->
           <div class="input-group">
             <input class="form-control form-control-login" style="border-right-style: none;" :type="visibility" placeholder="********" aria-describedby="addon-2" v-model="password" @keyup.enter="logIn()">
-            <span style="background: white;" class="input-group-addon password">
+            <span style="background: none;" class="input-group-addon password">
               <i v-if="visibility == 'password'" @click="showPassword()" class="fa fa-eye" aria-hidden="true"></i>
               <i v-if="visibility == 'text'" @click="hidePassword()" class="fa fa-eye-slash" aria-hidden="true"></i>
             </span>
           </div>
         </div>
-        <button class="btn btn-primary btn-block login-spacer" v-on:click="logIn()">Login</button>
-        <button class="btn btn-warning btn-block login-spacer" v-on:click="redirect('/request_reset_password')">Forgot your Password?</button>
+        <div class="input-group login-spacer mt-4 mb-4">
+          <button class="btn dropdown-toggle form-control select-btn" type="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            {{selected ? selected.address : 'Select location'}}
+          </button>
+          <div class="dropdown-menu btn-block ">
+            <b class="dropdown-item font-weight-normal select-item" href="#" v-for="(el, ndx) in locations" :key="String(ndx) + String(el.id)" @click="selectItem(el)">{{el.address}}</b>
+          </div>
+        </div>
+        <button class="btn btn-block login-spacer bg-white text-primary login-btn mt-4" v-on:click="logIn()">LOG IN</button>
+
+        <!-- <button class="btn btn-warning btn-block login-spacer" v-on:click="redirect('/request_reset_password')">Forgot your Password?</button> -->
         <br>
         <!-- <div class="container-fluid separator">
             or
@@ -43,115 +52,6 @@
     <authenticate-otp ref="authenticateOTP"></authenticate-otp>
   </div>
 </template>
-<style scoped lang="scss">
-@import "~assets/style/colors.scss";
-.holder{
-  margin-top: 25px;
-}
-
-.bg-primary{
-  background: $primary !important; 
-}
-
-.login-wrapper{
-  width: 80%;
-  margin: 0 10% 50px 10%;
-}
-
-.login-header{
-  height: 100px;
-  color: #006600;
-  width: 100%;
-  float: left;
-  text-align: center;
-}
-
-.login-header img{
-  height: 100px !important;
-  width: auto !important;
-}
-
-.login-header img:hover{
-  cursor: pointer;
-}
-
-.login-message-holder{
-  min-height: 30px;
-  font-size: 12px;
-  float: left;
-  overflow: hidden;
-}
-
-.login-spacer{
-  margin-bottom: 10px;
-}/*-- login-spacer --*/
-
-.forgot-password a{
-  color: #006600 !important;
-}
-.forgot-password a:hover{
-  cursor: pointer !important;
-  text-decoration: underline !important;
-  color: #009900 !important;
-}
-
-.btn{
-  height: 50px !important;
-}
-
-.input-group-addon{
-  width: 15% !important;
-  text-align: center !important;
-  padding: 0px !important;
-  display: block !important;
-  line-height: 43px !important;
-}
-
-.form-control-login{
-  height: 45px !important;
-}
-
-/*    Line with text on top  */
-.separator>*{
-  display: inline-block;
-  vertical-align: middle;
-}
-
-.separator {
-    text-align: center;
-    border: 0;
-    white-space: nowrap;
-    display: block;
-    overflow: hidden;
-    padding: 0;
-    margin: 0;
-}
-
-.separator:before, .separator:after {
-    content: "";
-    height: 1px;
-    width: 50%;
-    background-color: #ccc;
-    margin: 0 5px 0 5px;
-    display: inline-block;
-    vertical-align: middle;
-}
-
-.separator:before {
-    margin-left: -100%;
-}
-
-.separator:after {
-    margin-right: -100%;
-}
-
-@media (max-width: 992px){
-  .login-wrapper{
-    width: 96%;
-    margin: 0 2% 0 2%;
-  }
-}
-</style>
 <script>
 import ROUTER from 'src/router'
 import AUTH from 'src/services/auth'
@@ -169,13 +69,27 @@ export default {
       otpCode: null,
       otpErrorCode: null,
       common: COMMON,
-      visibility: 'password'
+      visibility: 'password',
+      locations: [
+        {
+          id: 1,
+          address: `8 Queen's Road W`
+        },
+        {
+          id: 2,
+          address: `Location 2`
+        }
+      ],
+      selected: null
     }
   },
   components: {
     'authenticate-otp': require('components/increment/generic/otp/Otp.vue')
   },
   methods: {
+    selectItem(item) {
+      this.selected = item
+    },
     showPassword() {
       this.visibility = 'text'
     },
@@ -231,3 +145,174 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+@import "~assets/style/colors.scss";
+.select-item {
+  cursor: pointer;
+}
+.select-btn {
+  font: 13px sans-serif;
+  padding: .5em 1em;
+  padding-top: .5em;
+  padding-bottom: .5em;
+  padding-left: 1em;
+  padding-right: 1.6em;
+  display: flex;
+  justify-content: space-between;
+  color: #16181a !important;
+  background: $white !important;
+  outline: none;
+  border-top-right-radius: 3px !important;
+  border-bottom-right-radius: 3px !important;
+}
+
+.select-btn::after {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  margin: -.3em 0 0 .4em;
+  vertical-align: middle;
+  content: "";
+  border: .3em solid;
+  border-width: 0 .15em .15em 0;
+  transform: rotateZ(45deg);
+  font-weight: 100 !important;
+}
+.login-btn {
+  color: $darkPrimary !important;
+  font-weight: bold;
+  font-size: 24px;
+}
+.login-container {
+  height: 100vh!important;
+  width:  100vw !important;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $primary;
+}
+.holder{
+  margin-top: 25px;
+}
+
+.bg-primary{
+  background: $primary !important; 
+}
+
+.login-wrapper{
+  width: 80%;
+  margin: 0 10% 50px 10%;
+}
+
+.login-header{
+  height: 250px;
+  color: #006600;
+  width: 100%;
+  float: left;
+  text-align: center;
+}
+
+.login-header img{
+  height: 250px !important;
+  width: auto !important;
+}
+
+.login-header img:hover{
+  cursor: pointer;
+}
+
+.login-message-holder{
+  font-size: 12px;
+}
+
+.login-spacer{
+  margin-bottom: 10px;
+}/*-- login-spacer --*/
+
+.forgot-password a{
+  color: #006600 !important;
+}
+.forgot-password a:hover{
+  cursor: pointer !important;
+  text-decoration: underline !important;
+  color: #009900 !important;
+}
+
+.btn{
+  height: 50px !important;
+}
+
+.input-group-addon{
+  width: 15% !important;
+  text-align: center !important;
+  padding: 0px !important;
+  display: block !important;
+  line-height: 43px !important;
+}
+
+.form-control-login{
+  height: 45px !important;
+  outline: $white;
+  border: 1px solid $white;
+  background: transparent !important;
+  color: $white;
+}
+.form-control-login::-webkit-input-placeholder { /* Edge */
+  color: $white;
+}
+
+.form-control-login:-ms-input-placeholder { /* Internet Explorer 10-11 */
+  color: $white;
+}
+
+.form-control-login::placeholder {
+  color: $white;
+}
+.password {
+  border-top: 1px solid $white;
+  border-right: 1px solid $white;
+  border-bottom: 1px solid $white;
+  color: $white;
+}
+
+/*    Line with text on top  */
+.separator>*{
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.separator {
+    text-align: center;
+    border: 0;
+    white-space: nowrap;
+    display: block;
+    overflow: hidden;
+    padding: 0;
+    margin: 0;
+}
+
+.separator:before, .separator:after {
+    content: "";
+    height: 1px;
+    width: 50%;
+    background-color: #ccc;
+    margin: 0 5px 0 5px;
+    display: inline-block;
+    vertical-align: middle;
+}
+
+.separator:before {
+    margin-left: -100%;
+}
+
+.separator:after {
+    margin-right: -100%;
+}
+
+@media (max-width: 992px){
+  .login-wrapper{
+    width: 96%;
+    margin: 0 2% 0 2%;
+  }
+}
+</style>
